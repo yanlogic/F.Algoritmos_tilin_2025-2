367d294f0f99bc859c8e94427cd9aa7d885ad2e0
Jeremy <jeremy.jsolorzano@gmail.com>
2025-09-09 14:52:25 -0500

    ejer2 sem3

--- Semana1_Python/Semana1_Python.py
+++ Semana1_Python/Semana1_Python.py
@@ -3,4 +3,11 @@ def ejer1(): #creando método ejer1
     carrera = input("Ingrese su carrera: ")
     print(nombre, ", bienvenido al curso de Fundamentos de Algoritmos de la carrera ", carrera)
 
-ejer1()
+def ejer2():
+    x = int(input("Ingrese el valor de x: "))
+    y = int(input("Ingrese  el valor de y: "))
+
+    print("suma: ", (x+y))
+    print("resta: ", (x-y))
+    print("multiplicación: ", (x*y))
+    print("división: ", (x/y))
