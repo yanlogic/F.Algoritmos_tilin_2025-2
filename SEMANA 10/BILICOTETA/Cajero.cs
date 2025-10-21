8bea33232bbfb06ffd07e828196027fe7df9ee58
yanela <yannw7e@gmail.com>
2025-10-21 15:51:59 -0500

    SUBO SEMANA 10 POR FIN

---
+++ SEMANA 10/BILICOTETA/Cajero.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BILICOTETA
+{
+    public class Cajero
+    {
+        double saldo = 1000;
+
+        public double Consultar()
+        {
+            return saldo;
+        }
+
+        public void Depositar(double monto)
+        {
+            while(true)
+            {
+                Console.Write("\nIngrese el monto a depositar: ");
+                string en = Console.ReadLine();
+                try
+                {
+                    monto = Convert.ToDouble(en);
+
+                    if (monto > 0)
+                    {
+                        saldo += monto;
+                        Console.WriteLine("Depósito exitoso.");
+                        break;
+                    }
+                    else
+                    {   
+                        Console.WriteLine("Error. Tiene que ser un depósito mayor a 0");
+                        continue;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nError. El depósito tiene que ser un monto válido");
+
+                }
+            }
+        }
+        public void Retirar(double monto)
+        {
+            while(true)
+            {
+                Console.Write("\nIngrese el monto a retirar: ");
+                string en = Console.ReadLine();
+                try
+                {
+                    monto = Convert.ToDouble(en);
+
+                    if (monto <= saldo)
+                    {
+                        saldo -= monto;
+                        Console.WriteLine("Retiro exitoso.");
+                        break;
+                    }
+                    else if (monto < 0)
+                    {
+                        Console.WriteLine("Error. Tiene que ser un retiro mayor a 0");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error. Saldo insuficiente");
+                        continue;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nError. El retiro tiene que ser un monto válido");
+
+                }
+            }
+        }
+    }
+}
---
+++ SEMANA 10/ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BILICOTETA;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cajero c = new Cajero();
+            int opc;
+            string conti;
+
+            do
+            {
+                Console.WriteLine("BIENVENIDO AL SISTEMA DE CAJERO\n");
+                Console.WriteLine("1. Consultar saldo");
+                Console.WriteLine("2. Depositar dinero");
+                Console.WriteLine("3. Retirar dinero");
+                Console.WriteLine("4. Salir\n");
+
+                do
+                {
+                    Console.Write("Ingrese una opción: ");
+                    opc = int.Parse(Console.ReadLine());
+                    if (opc <= 0 | opc > 4)
+                    {
+                        Console.WriteLine("Opción inválida. Ingrese nuevamente\n");
+                    }
+
+                }
+                while (opc < 0 | opc > 4);
+
+                switch (opc)
+                {
+                    case 1: Console.WriteLine("Saldo disponible: S/" + c.Consultar()); break;
+                    case 2: c.Depositar(0); break;
+                    case 3: c.Retirar(0); break;
+                    case 4: return;
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("\n¿Desea continuar? (S/N): ");
+                    conti = Console.ReadLine();
+
+                    if (conti != "s" & conti != "n")
+                    {
+                        Console.WriteLine("Error. Sólo se permiten 'S' o 'N'.");
+                    }
+                    else break;
+                }
+                Console.Clear();
+            }
+            while (conti == "s");
+        }
+    }
+}
