74f93213f383552de39bcd47f53300e15793bca4
yanela <yannw7e@gmail.com>
2025-11-18 11:05:02 -0500

    subo semana 14

---
+++ semana 14/semana 14/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arreglopar = new int[10,5];
+            int acumulador = 2;
+
+            for (int i = 0; i < 10; i ++)
+            {
+                for (int j = 0; j < 5; j ++)
+                {
+                    arreglopar[i,j] = acumulador;
+                    acumulador += 2;
+                }
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(arreglopar[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ semana 14/semana 14/Ejercicio2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] arreglodatos = new string[3,2];
+
+            arreglodatos[0, 0] = "CURSO: ";
+            arreglodatos[1, 0] = "DOCENTE: ";
+            arreglodatos[2, 0] = "PRECIO: ";
+
+            for (int i = 0; i < 3; i++) 
+            {
+                Console.Write(arreglodatos[i, 0]);
+
+                for (int j = 1; j < 2; j++) 
+                {
+                    Console.Write("");
+                    arreglodatos[i, j] = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=== LISTADO ===");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(arreglodatos[i, 0]);
+
+                for (int j = 1; j < 2; j++)
+                {
+                    Console.Write(arreglodatos[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
